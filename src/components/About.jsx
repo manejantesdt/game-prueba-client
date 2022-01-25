73bba2af69238f08fa23cf9903b559c6e642d494
pivotsk8 <the_pivotsk8@hotmail.com>
2022-01-25 10:53:29 -0500

    componentes

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+
+export const About = () => {
+  return (
+    <div>
+        soy un jueguito divertido
+        <Link to="/">home</Link>
+    </div>
+  );
+};
