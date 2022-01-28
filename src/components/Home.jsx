92d061232c5e4eda30ed84c9122059adbdcff75b
pivotsk8 <the_pivotsk8@hotmail.com>
2022-01-28 09:50:12 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@
 import Players from "./Players";
 import Login from "./Login";
 import MainPanel from "./MainPanel";
-import NavBar from "./NavBar";
 import "../styles/styles.scss";
 
 
@@ -12,7 +11,6 @@ import "../styles/styles.scss";
 export const Home = () => {
   return (
     <main>
-      <NavBar />
       <Players />
       <MainPanel />
       <Login />
