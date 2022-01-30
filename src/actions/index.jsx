a25961216e80f9118210879d3406e8053ebfaccf
pivotsk8 <the_pivotsk8@hotmail.com>
2022-01-30 14:37:55 -0500

    arreglos action

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,24 +1,45 @@
 import axios from "axios";
 
-/* export const CREATE_PLAYER = "CREATE_PLAYER" - BORRAR - no hace falta esto */
+// -----------------------<Gets>------------------------
 
-export const createPlayer = (payload) => {
-    console.log(payload,"payload")
-    return {
-        type: "CREATE_PLAYER",
-        payload: payload
+export const getPlayers = ({ nick_name, order, status }) => {
+  return async (dispatch) => {
+    try {
+      var json = (
+        await axios.get(
+          `http://localhost:3001/player?nickname=${
+            nick_name ? nick_name : ""
+          }&order=${order ? order : ""}&status=${status ? status : ""}`
+        )
+      ).data;
+      dispatch({
+        type: "GET_PLAYERS",
+        payload: json,
+      });
+    } catch (error) {
+      console.error(error);
     }
-}
-
-
-export function getAvatar() {
-    return async function (dispatch) {
-        var json = await axios.get("http://localhost:3001/avatar/");
-        return dispatch({
-            type: "GET_AVATAR",
-            payload: json.data
-        })
-    }
+  };
 };
 
+export const getAvatar = () => {
+  return async (dispatch) => {
+    try {
+      var json = (await axios.get("http://localhost:3001/avatar/")).data;
+      dispatch({
+        type: "GET_AVATAR",
+        payload: json,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+// ______________________________________________________
 
+export const createPlayer = (payload) => {
+  return {
+    type: "CREATE_PLAYER",
+    payload: payload,
+  };
+};
