9206e1c6092cc600d7faba9f4f2b133bd124cfc1
Gustavo Montero <79276560+gamontero@users.noreply.github.com>
2022-02-15 19:18:38 -0300

    Componente loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,10 @@
+
+import React from "react";
+import loading from "../img/294.svg";
+
+export const Loading = () => (
+  <div className="spinner">
+    <img src={loading} alt="Loading" />
+  </div>
+);
+
