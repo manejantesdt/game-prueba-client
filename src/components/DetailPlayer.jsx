2bb74ab0fe3211d22e861aa10b62359c1bcdd881
pivotsk8 <the_pivotsk8@hotmail.com>
2022-02-17 19:22:17 -0500

    Update DetailPlayer.jsx
    se actualizo, el commponente para renderizar , correctamente la informacion

--- src/components/DetailPlayer.jsx
+++ src/components/DetailPlayer.jsx
@@ -6,7 +6,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 import {
   getPlayerId,
   searchPlayers,
-  // setId
 } from "../actions/index";
 import { EditPlayer } from "./EditPlayer.jsx";
 
@@ -14,7 +13,7 @@ export const DetailPlayer = () => {
   // ------------------------------<Variables>--------------------------------
   var { player } = useSelector((state) => state);
   player = player[0];
-  console.log(player);
+  
   const dispatch = useDispatch();
   var { id } = useParams();
   const { user, isAuthenticated } = useAuth0();
@@ -28,19 +27,18 @@ export const DetailPlayer = () => {
     ) {
       setAdminStatus(true);
     }
-  }, [dispatch,player?.score||player?.nickname]);
+  }, [dispatch,player?.score,player?.nickname,player?.length]);
   // _____________________________________________________________________________
   // ------------------------------<State>----------------------------------
   const [checkform, setCheckform] = useState(false);
   const [adminStatus, setAdminStatus] = useState(false);
   const onClickCheck = () => {
     checkform === true ? setCheckform(false) : setCheckform(true);
-    // dispatch(setId(id))
   };
 
   // __________________________________________________________________________
 
-  return player?.nickname ? (
+return player? (
     checkform === false ? (
       <>
         <h2
@@ -50,7 +48,6 @@ export const DetailPlayer = () => {
             color: "white",
             textAlign: "center",
             marginTop: 60,
-            // color: "#FF0075",
             letterSpacing: 20,
             textTransform: "uppercase",
           }}
