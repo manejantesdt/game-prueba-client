78f261189a221584ee6592687c378dea115515ad
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2022-02-19 09:56:22 -0400

    Create Spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyledSpinner } from "../styles/Spinner";
+
+export default function Spinner() {
+  return (
+    <StyledSpinner>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </StyledSpinner>
+  );
+}
