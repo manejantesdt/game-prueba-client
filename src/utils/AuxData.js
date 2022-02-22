de00c72a26360f5593b90d6e990e0d7f6f6fe3f8
manejantesdt <98234037+manejantesdt@users.noreply.github.com>
2022-02-21 21:55:04 -0500

    Revert "Update AuxData.js"

--- src/utils/AuxData.js
+++ src/utils/AuxData.js
@@ -1,27 +1,36 @@
-import avatar1 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281389/proyecto%20creitu/avatar10_mhxh5j.png";
-import avatar2 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281389/proyecto%20creitu/avatar7_gowzmy.png";
-import avatar3 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281389/proyecto%20creitu/avatar11_rxmj6t.png";
-import avatar4 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281389/proyecto%20creitu/avatar9_dmnven.png";
-import avatar5 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281389/proyecto%20creitu/avatar12_qqx6m9.png";
-import avatar6 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281389/proyecto%20creitu/avatar8_c1vbs9.png";
-import avatar7 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281388/proyecto%20creitu/avatar6_gaom4k.png";
-import avatar8 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281388/proyecto%20creitu/avatar1_ubus6h.png";
-import avatar9 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281388/proyecto%20creitu/avatar2_tzfujk.png";
-import avatar10 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281388/proyecto%20creitu/avatar4_x70myg.png";
-import avatar11 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281388/proyecto%20creitu/avatar3_k1tusr.png";
-import avatar12 from "https://res.cloudinary.com/dlwobuyjb/image/upload/v1645281388/proyecto%20creitu/avatar5_p4ulbz.png";
+import avatar1 from '../assets/img/Avatars/avatar1.png';
+import avatar2 from '../assets/img/Avatars/avatar2.png';
+import avatar3 from '../assets/img/Avatars/avatar3.png';
+import avatar4 from '../assets/img/Avatars/avatar4.png';
+import avatar5 from '../assets/img/Avatars/avatar5.png';
+import avatar6 from '../assets/img/Avatars/avatar6.png';
+import avatar7 from '../assets/img/Avatars/avatar7.png';
+import avatar8 from '../assets/img/Avatars/avatar8.png';
+import avatar9 from '../assets/img/Avatars/avatar9.png';
+import avatar10 from '../assets/img/Avatars/avatar10.png';
+import avatar11 from '../assets/img/Avatars/avatar11.png';
+import avatar12 from '../assets/img/Avatars/avatar12.png';
 
-export const avatars = [
-  avatar1,
-  avatar2,
-  avatar3,
-  avatar4,
-  avatar5,
-  avatar6,
-  avatar7,
-  avatar8,
-  avatar9,
-  avatar10,
-  avatar11,
-  avatar12,
-];
+     
+
+  export const avatars = [
+    avatar1,
+    avatar2,
+    avatar3,
+    avatar4,
+    avatar5,
+    avatar6,
+    avatar7,
+    avatar8,
+    avatar9,
+    avatar10,
+    avatar11,
+    avatar12,
+  ];
+
+
+
+
+
+
+
