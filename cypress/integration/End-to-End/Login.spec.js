881afd05c9d29820004a9f6eecaf59721204299e
Gustavo Montero <gusmontero@gmail.com>
2022-02-21 21:24:12 -0300

    Update Login.spec.js

--- cypress/integration/End-to-End/Login.spec.js
+++ cypress/integration/End-to-End/Login.spec.js
@@ -1,6 +1,5 @@
 describe('access secret admin functionality', () => {
     it('should be able to navigate to', () => {
-      cy.visitHome().loginAsAdmin()
-      
+      cy.visitHome().loginAsAdmin()     
    });
   });
